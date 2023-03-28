3ca3bdcde9f7b09f701891736f8edc378a260017
hima4002 <125842766+hima4002@users.noreply.github.com>
2023-03-28 15:59:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -12,13 +12,7 @@ headerRow.insertCell().innerText = 'P Value';
 headerRow.insertCell().innerText = 'EVA DQ';
 
 // Configure KafkaJS
-const { Kafka } = require('kafkajs');
-const kafka = new Kafka({
-  brokers: ['localhost:9092'],
-  clientId: 'my-app',
-});
-
-// Subscribe to the Kafka topic
+const kafka = window.KafkaJS;
 const consumer = kafka.consumer({ groupId: 'my-group' });
 async function run() {
   await consumer.connect();
@@ -28,18 +22,24 @@ async function run() {
   await consumer.run({
     eachMessage: async ({ message }) => {
       // Parse the JSON data from the message
-      const data = JSON.parse(message.value.toString());
+      let data = message.value.toString();
+
+      try {
+        data = JSON.parse(data);
+      } catch (error) {
+        // Do nothing, assume data is already in the correct format
+      }
 
       // Create a row for the data in the HTML table
       const row = table.insertRow();
 
       // Set the cell values for the row
-      row.insertCell().innerText = data.memberNumber;
-      row.insertCell().innerText = data.callProbability;
-      row.insertCell().innerText = data.rawPageTags;
+      row.insertCell().innerText = data.memberNumber || data.member_number;
+      row.insertCell().innerText = data.callProbability || data.call_probability;
+      row.insertCell().innerText = data.rawPageTags || data.raw_page_tags;
       row.insertCell().innerText = data.intent;
-      row.insertCell().innerText = data.p_value;
-      row.insertCell().innerText = data.EVA_DQ;
+      row.insertCell().innerText = data.p_value || data.pValue;
+      row.insertCell().innerText = data.EVA_DQ || data.evaDq;
 
       // Keep the table size within a limit by removing the oldest row(s) if necessary
       const maxRows = 1000;
