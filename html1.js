d7412b9bedcb604be77ed8953e6405f12892657b
hima4002 <125842766+hima4002@users.noreply.github.com>
2023-03-28 19:03:55 -0400

    Create html1.js

---
+++ html1.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Kafka Consumer</title>
+    <script src="path/to/your/kafkajs-library/index.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/kafkajs@2.1.0/dist/kafka.min.js"></script>
+  </head>
+  <body>
+    <table id="my-table">
+      <thead>
+        <tr>
+          <th>Member Number</th>
+          <th>Call Probability</th>
+          <th>Raw Page Tags</th>
+          <th>Intent</th>
+          <th>P Value</th>
+          <th>EVA DQ</th>
+        </tr>
+      </thead>
+    </table>
+    <script src="path/to/your/app.js"></script>
+  </body>
+</html>
